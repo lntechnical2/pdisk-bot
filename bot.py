de22df760befb97510e800b20630535d27ce7b0e
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-05 22:30:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,14 +9,14 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 
-APP_ID = int(os.environ.get("APP_ID", "12345"))
+API_ID = int(os.environ.get("APP_ID", "12345"))
 
 API_HASH = os.environ.get("API_HASH", "")
 
 app = Client(
         "pdisk_bot",
         bot_token=TOKEN,
-        api_id=APP_ID,
+        api_id=API_ID,
         api_hash=API_HASH,
     )
     

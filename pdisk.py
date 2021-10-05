cfd725a1bfe13ac08fc92e99c36d5aaeedb0cd9e
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-05 13:18:19 +0530

    Update pdisk.py

--- pdisk.py
+++ pdisk.py
@@ -1,1 +1,30 @@
-
+import requests as re
+import json 
+def pdisk_url(api_key:str , link:str , title :str , thumb = None ):
+		if thumb is None :
+			data = { "api_key":api_key,
+	                   "content_src":link,
+	                    "link_type":"link",
+	                     "title": title 
+	                  } 	
+			res = re.post("http://linkapi.net/open/create_item",data).content
+			result = json.loads(res)
+			try:
+				id = result["data"]['item_id']
+			except:
+				id = result["msg"]
+			return id 
+		else:
+			data = { "api_key":api_key,
+	                   "content_src":link,
+	                    "link_type":"link",
+	                     "title": title ,
+	                     "cover_url":thumb
+	                  }
+			res = re.post('http://linkapi.net/open/create_item',data).content
+			result = json.loads(res)
+			try:
+				id = result["data"]['item_id']
+			except:
+				id = result["msg"]
+			return id                                                                    

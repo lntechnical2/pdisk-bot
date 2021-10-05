c3a1e4d79d00d427214652caa6a0d8daad8af6de
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-05 10:45:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,21 @@
+import pymongo 
+import os
 
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["USER"]
+
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"api_key":None}
+            try:
+            	dbcol.insert_one(user_det)
+            except:
+            	pass
+def find(chat_id)
+	user_id = chat_id
+	data = dbcol.find_one({"_id":user_id})
+	api_key = data["api_key"]
+	return api_key

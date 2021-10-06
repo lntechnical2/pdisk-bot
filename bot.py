2956cb1fff3733b451c8af285bf763a93ec8432b
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-06 11:50:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 from pyrogram import Client, filters
-from time import time
-from datetime import datetime
 from pyrogram.types import (
     ForceReply
 )
@@ -8,74 +6,24 @@ from pdisk import pdisk_url , api_check
 from database import insert , find , set , getid
 import os
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ('week', 60 * 60 * 24 * 7),
-    ('day', 60 * 60 * 24),
-    ('hour', 60 * 60),
-    ('min', 60),
-    ('sec', 1)
-)
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return 'inf'
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "s"))
-    return ', '.join(parts)
 
 TOKEN = os.environ.get("TOKEN", "")
 API_ID = int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH","")
-ADMIN = os.environ.get("ADMIN",923943045)
+ADMIN = os.environ.get("ADMIN","")
 
 app = Client("pdisk" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
     
-@Client.on_message(filters.command("start"))
-async def start(client, m: message):
-   if m.chat.type == 'private':
-       insert(int(m.chat.id))
-       await m.reply_text(f"**Hello i am Pdisk Uploader Bot\nMade with love by @mrlokaman`",   
-                            reply_markup=InlineKeyboardMarkup(
-                                [[
-                                     InlineKeyboardButton(
-                                            "📢 Bot Updates", url="https://t.me/lntechnical")
-                                    ]]
-                            ))
-   else:
-      await m.reply_text(f"**Hola, Join @lntechnical!**")
+@app.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	insert(int(message.chat.id))
+	await message.reply_text("Hello iam Pdisk Uploader Bot\nMade with love by @mrlokaman",reply_to_message_id = message.message_id)
 
 @app.on_message(filters.private & filters.command(['connect']))
 async def connect(client,message):
 	await message.reply_text('Send Me Your api_key from pdisk\nhttps://www.cofilink.com/use-api',reply_to_message_id = message.message_id,reply_markup=ForceReply(True))
+	            
 
-@Client.on_message(filters.command("ping"))
-async def ping_pong(client, m: message):
-    start = time()
-    m_reply = await m.reply_text("pinging...")
-    delta_ping = time() - start
-    await m_reply.edit_text(
-        "🏓 `PONG!!`\n"
-        f"⚡️ `{delta_ping * 1000:.3f} ms`"
-    )
-
-
-@Client.on_message(filters.command("uptime"))
-async def get_uptime(client, m: message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-    await m.reply_text(
-        "🤖 bot status:\n"
-        f"• **uptime:** `{uptime}`\n"
-        f"• **start time:** `{START_TIME_ISO}`"
-    )
-	
 @app.on_message(filters.private & filters.reply)
 async def api_connect(client,message):
         if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):

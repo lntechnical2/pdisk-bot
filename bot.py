e43d47be520728534960d2ff2ebd95d2701ec526
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-06 11:37:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def _human_time_duration(seconds):
 TOKEN = os.environ.get("TOKEN", "")
 API_ID = int(os.environ.get("API_ID",12345))
 API_HASH = os.environ.get("API_HASH","")
-ADMIN = os.environ.get("ADMIN","")
+ADMIN = os.environ.get("ADMIN",923943045)
 
 app = Client("pdisk" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
     

09fe05d67f0cf2b8d1ca8826e9e7890031b43419
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-07 12:33:06 +0530

    Create broadcast.py

---
+++ plugins/broadcast.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters
+ADMIN = int(os.environ.get("ADMIN", 923943045))
+
+@app.on_message(filters.user(ADMIN) & filters.command(["broadcast"]))
+async def broadcast(bot, message):
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	await message.reply_to_message.copy(id)
+     except:
+     	pass

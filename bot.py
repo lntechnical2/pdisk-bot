31eb33df2c3006be080ad2661ab10cf746ef51f5
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-07 12:31:01 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID",12345))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "pdisk",
+        bot_token=TOKEN,
+        api_hash=API_HASH,
+        api_id=API_ID,
+        plugins=plugins
+    )
+    app.run()

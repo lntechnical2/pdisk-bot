0b252e8f6dde88e94dd7b1a2ef3746932e234b41
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-07 13:02:44 +0530

    Create file_test.py

---
+++ plugins/file_test.py
@@ -0,0 +1,11 @@
+from pyrogram import Client, filters
+
+@client.on_message(filters.private &(filters.document | filters.video))
+async def file_down(client,message):
+	file = message
+	ms = await message.repy_text("``` Trying To Download...```")
+	try:
+	   path = await client.download_media(message = file)
+	   await ms.edit(f"{path}")
+	except Exception as e:
+		await ms.edit(e)

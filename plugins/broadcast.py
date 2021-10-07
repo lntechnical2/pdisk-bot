61ad2f77582a7d2da4111d4b3e94e719614e8c39
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-07 12:39:07 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 import os
 ADMIN = int(os.environ.get("ADMIN", 923943045))
 
-@app.on_message(filters.user(ADMIN) & filters.command(["broadcast"]))
+@Client.on_message(filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
    ms = await message.reply_text("Geting All ids from database ...........")

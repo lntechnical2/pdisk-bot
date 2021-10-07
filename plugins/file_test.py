17f8e33267a320f0b11bed9a5e906fb1b7e0af80
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-07 13:06:46 +0530

    Update file_test.py

--- plugins/file_test.py
+++ plugins/file_test.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 @Client.on_message(filters.private &(filters.document | filters.video))
 async def file_down(client,message):
 	file = message
-	ms = await message.repy_text("``` Trying To Download...```")
+	ms = await message.reply_text("``` Trying To Download...```")
 	try:
 	   path = await client.download_media(message = file)
 	   await ms.edit(f"{path}")

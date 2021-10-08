afb47f513d6ab93c4e0a5206e99f76c786214df5
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-10-08 09:47:16 +0530

    Now It will look better
    All tested so No Eroor

--- plugins/mainfuc.py
+++ plugins/mainfuc.py
@@ -1,20 +1,64 @@
 from pyrogram import Client, filters
+from time import time
+from datetime import datetime
 from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
-    ForceReply
+    ForceReply,
+    Message
 )
 from pdisk import pdisk_url , api_check
 from database import insert , find , set , getid
 import os
 
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
 
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text("Hello iam Pdisk Uploader Bot\nMade with love by @mrlokaman",reply_to_message_id = message.message_id,reply_markup=InlineKeyboardMarkup([ [ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], [InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]  ) 
         )
+	
+@Client.on_message(filters.command("ping"))
+async def ping_pong(client, m: Message):
+    start = time()
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "🏓 `PONG!!`\n"
+        f"⚡️ `{delta_ping * 1000:.3f} ms`"
+    )
+
+
+@Client.on_message(filters.command("uptime"))
+async def get_uptime(client, m: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await m.reply_text(
+        "🤖 Bot status:\n"
+        f"• **Uptime:** `{uptime}`\n"
+        f"• **Start time:** `{START_TIME_ISO}`"
+    )   
 
 @Client.on_message(filters.private & filters.command(['connect']))
 async def connect(client,message):
@@ -72,5 +116,4 @@ async def upload(client,message):
 				await message.reply_text(f"Error:```{e}```",reply_to_message_id = message.message_id)
 			
 	else:
-		await message.reply_text("Connect Your Account Using Command /connect",reply_to_message_id = message.message_id)	
-		
+		await message.reply_text("Connect Your Account Using Command /connect",reply_to_message_id = message.message_id)
